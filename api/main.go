dc71f477ee23194400be5071712552c35dd784c6
Boyan Yordanov <boyan.yordanov.dev@gmail.com>
2023-04-19 12:36:35 +0300

    Moved NATS Jetstream config outside the handler function in the api so it can be created before the first requests. Adjusted docker compose config to mak the cmd instances wait for the API to start and create the stream before trying to subscribe

--- api/main.go
+++ api/main.go
@@ -10,6 +10,21 @@ import (
 )
 
 func main() {
+
+	// Set up the NATS connection and stream
+	nc, _ := nats.Connect(os.Getenv("NATS_SERVER"))
+	js, _ := nc.JetStream()
+
+	// Create a Stream
+	_, err := js.AddStream(&nats.StreamConfig{
+		Name:     "NOTIFICATIONS",
+		Subjects: []string{"notifications.>"},
+	})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
 	// Create a new handler for the root path
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		_, err := w.Write([]byte("Try creating a notification with POST /notifications."))
@@ -19,59 +34,47 @@ func main() {
 	})
 
 	// Attach a function as handler for the more complicated endpoint
-	http.HandleFunc("/notifications", handleNotificationCreation)
+	http.HandleFunc("/notifications", handleNotificationCreation(js))
 
 	// Start the server
 	log.Println("Listening on http://localhost:8000")
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
-func handleNotificationCreation(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		var notification notifications.Notification
-		err := json.NewDecoder(r.Body).Decode(&notification)
+func handleNotificationCreation(js nats.JetStream) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			var notification notifications.Notification
+			err := json.NewDecoder(r.Body).Decode(&notification)
 
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("Error parsing JSON"))
-			return
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte("Error parsing JSON"))
+				return
+			}
+
+			resp, _ := json.Marshal(notification)
+
+			// Publish the notification to the queue
+			pubAck, pubErr := js.Publish("notifications.1", resp)
+
+			if pubErr != nil {
+				log.Println(pubErr)
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte("Error sending notification"))
+				return
+			}
+
+			log.Println(pubAck)
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusAccepted)
+			w.Write([]byte(resp))
+		} else {
+			// We're only interested in creating notifications for this project so everything else will be marked as not implemented
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusNotImplemented)
+			w.Write([]byte("{\"error\": \"Not implemented. Only available operation is POST.\"}"))
 		}
-
-		resp, _ := json.Marshal(notification)
-
-		// Set up the NATS connection and stream
-		nc, _ := nats.Connect(os.Getenv("NATS_SERVER"))
-		js, _ := nc.JetStream()
-
-		// Create a Stream
-		_, err = js.AddStream(&nats.StreamConfig{
-			Name:     "NOTIFICATIONS",
-			Subjects: []string{"notifications.>"},
-		})
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		// Publish the notification to the queue
-		pubAck, pubErr := js.Publish("notifications.1", resp)
-
-		if pubErr != nil {
-			log.Println(pubErr)
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Error sending notification"))
-			return
-		}
-
-		log.Println(pubAck)
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusAccepted)
-		w.Write([]byte(resp))
-	} else {
-		// We're only interested in creating notifications for this project so everything else will be marked as not implemented
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusNotImplemented)
-		w.Write([]byte("{\"error\": \"Not implemented. Only available operation is POST.\"}"))
 	}
 }
